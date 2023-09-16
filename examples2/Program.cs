5010a084aede6f2d528ac97ff55612af61e268b6
AlinaBitochkina <alina.bitochkina96@mail.ru>
2023-09-16 17:44:23 +0500

    second comment

---
+++ examples2/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("write your name, motherfucker");
+        string username = Console.ReadLine();
+        Console.WriteLine("hello, ");
+        Console.WriteLine(username);
+    }
+}
